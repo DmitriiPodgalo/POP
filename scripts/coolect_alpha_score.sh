5a3bb0ce115e1f5ca3362dca1ebc00d292301935
DmitriiPodgalo <77990014+DmitriiPodgalo@users.noreply.github.com>
2022-05-28 20:40:28 +0300

    Update coolect_alpha_score.sh

--- scripts/coolect_alpha_score.sh
+++ scripts/coolect_alpha_score.sh
@@ -2,8 +2,8 @@
 
 for i in $(seq 1 1339);
 do
-	        path='../../../alpha_fold/'`cut -f 8 ../sum.tsv | sed "$i!d"`
-		        pos=`cat ../sum.tsv | cut -f 4 | sed "$i!d"`
-			        echo $path
-				        echo `zcat $path | grep '^ATOM' | awk '{print $6, $11}' | uniq | grep "^$pos " | cut -f 2` >> score.tsv
-				done
+	path='../../../alpha_fold/'`cut -f 8 ../sum.tsv | sed "$i!d"`
+	pos=`cat ../sum.tsv | cut -f 4 | sed "$i!d"`
+	echo $path
+	echo `zcat $path | grep '^ATOM' | awk '{print $6, $11}' | uniq | grep "^$pos " | cut -f 2` >> score.tsv
+done
