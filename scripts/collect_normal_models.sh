7d6455aacab6048cd3e792814496d3546efd9b8d
DmitriiPodgalo <77990014+DmitriiPodgalo@users.noreply.github.com>
2022-05-28 20:40:09 +0300

    Update collect_normal_models.sh

--- scripts/collect_normal_models.sh
+++ scripts/collect_normal_models.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq 1 1339);
 do
-	        path='path/to/alpha_fold/'`cut -f 8 sum.tsv | sed "$i!d"` # prepare path to normal model
-		        echo $path                                                # logging
-			        `cp $path ./norm_models/`                                 # copy the model to the folder
-			done
+	path='path/to/alpha_fold/'`cut -f 8 sum.tsv | sed "$i!d"` # prepare path to normal model
+	echo $path                                                # logging
+	`cp $path ./norm_models/`                                 # copy the model to the folder
+done
