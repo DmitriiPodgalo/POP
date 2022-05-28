7511bf97f94221c86caa1454831204e7bcab1271
DmitriiPodgalo <77990014+DmitriiPodgalo@users.noreply.github.com>
2022-05-28 20:42:16 +0300

    Update vep_filter.sh

--- scripts/vep_filter.sh
+++ scripts/vep_filter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 filter_vep --input_file exac.vep.gz \
-	           --output_file exac.filt.vep \
-		              --filter "Consequence is missense_variant and (Amino_acids matches /S or Amino_acids matches /C)"
+	   --output_file exac.filt.vep \
+	   --filter "Consequence is missense_variant and (Amino_acids matches /S or Amino_acids matches /C)"
