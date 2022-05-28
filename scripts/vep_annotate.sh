240ff2c9fe70608298ea2e39ac96ca09c97e143d
DmitriiPodgalo <77990014+DmitriiPodgalo@users.noreply.github.com>
2022-05-28 20:41:56 +0300

    Update vep_annotate.sh

--- scripts/vep_annotate.sh
+++ scripts/vep_annotate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 vep --input_file exac.vcf.gz \
-	    --output_file exac.vep.gz \
-	        --compress_output gzip \ # compress results
-		    --offline \              # use local database
-		        --GRCh37 \               # use GRCh37 human assembly
-			    --custom clinvar.vcf.gz,ClinVar,vcf,exact,0,CLNSIG,CLNREVSTAT,CLNDN,CLNDNINCL,CLNDISDB,CLNDISDBINCL,CLNSIGCONF,CLNSIGINCL,CLNVC,RS \ # use clinvar
-			        --uniprot \              # add uniprot id
-				    --fork 40                # parallel execution
+	--output_file exac.vep.gz \
+	--compress_output gzip \ # compress results
+	--offline \              # use local database
+	--GRCh37 \               # use GRCh37 human assembly
+	--custom clinvar.vcf.gz,ClinVar,vcf,exact,0,CLNSIG,CLNREVSTAT,CLNDN,CLNDNINCL,CLNDISDB,CLNDISDBINCL,CLNSIGCONF,CLNSIGINCL,CLNVC,RS \ # use clinvar
+	--uniprot \              # add uniprot id
+	--fork 40                # parallel execution
